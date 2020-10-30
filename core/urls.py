c0f88a3628f21ae8195e83d1aee9a8485c46a954
hophoet <hophoet@gmail.com>
2020-10-30 23:00:51 +0000

    add view access url path (cart, add single order item to cart, remove single order item from cart

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('<int:pk>/', views.ItemDetailView.as_view(), name='item_detail'),
     path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<int:id>/', views.remove_from_cart, name='remove_from_cart'),
-    
-
+    path('cart/', views.CartView.as_view(), name='cart'),
+    path('add-single-order-item-to-cart/<int:id>/', views.add_single_order_item_to_cart, name='add_single_order_item_to_cart' ),
+    path('remove-single-order-item-from-cart/<int:id>/', views.remove_single_order_item_from_cart, name='remove_single_order_item_from_cart')
 
 ]
