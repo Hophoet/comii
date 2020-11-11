3d4ba749999873d8be5174a82465c55110503a53
hophoet <hophoet@gmail.com>
2020-11-11 01:40:36 +0000

    add models (Order, BillingAddress, Payment) to the django admin site

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Item, Cart, OrderItem, Cart 
+from .models import Item, Cart, OrderItem, Cart, Order, BillingAddress, Payment
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Cart)
 admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(BillingAddress)
+admin.site.register(Payment)
 # admin.site.register()
