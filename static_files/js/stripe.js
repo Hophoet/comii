f865878be145b0eb69eca0415fc8f68506213278
hophoet <hophoet@gmail.com>
2020-11-14 01:20:35 +0000

    update stripe js client side script

--- static_files/js/stripe.js
+++ static_files/js/stripe.js
@@ -1,5 +1,5 @@
 // Create a Stripe client.
-var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+var stripe = Stripe('pk_test_51HjXyDKvYCb6qstMpnORUwLDScnAXkO53G1IOPULIzbKkF7DOsxdDAC6I2LnNUj0YagJSFwAUXMES2N5oJEAY6se00XZAZdkcp');
 
 // Create an instance of Elements.
 var elements = stripe.elements();
@@ -41,11 +41,12 @@ card.on('change', function(event) {
 // Handle form submission.
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
-  console.log("SUBMIT")
+  // console.log("SUBMIT")
   event.preventDefault();
-
   stripe.createToken(card).then(function(result) {
+    // console.log('RESULT', result)
     if (result.error) {
+      // console.log('ERROR', result.error )
       // Inform the user if there was an error.
       var errorElement = document.getElementById('card-errors');
       errorElement.textContent = result.error.message;
@@ -62,10 +63,11 @@ function stripeTokenHandler(token) {
   var form = document.getElementById('payment-form');
   var hiddenInput = document.createElement('input');
   hiddenInput.setAttribute('type', 'hidden');
-  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('name', 'card-code');
   hiddenInput.setAttribute('value', token.id);
   form.appendChild(hiddenInput);
 
+  console.log('READY FOR SUBMIT', form)
   // Submit the form
   form.submit();
 }
