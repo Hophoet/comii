8886729b4efb7a24390b708f8f561a060840cb2f
hophoet <hophoet@gmail.com>
2020-11-15 15:17:27 +0000

    update ALOWED_HOSTS , add Procfile

--- comii/settings.py
+++ comii/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'yh_!j&ht$o5qmzct#kj+o2my*$nhia#$vyrcl7f=*7_^d$0^c!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hophoetcomii.herokuapp.com', '127.0.0.1']
+
 
 
 # Application definition
