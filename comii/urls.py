7fc50212bd0b0528da0753424e3bb36ad0db16b4
hophoet <hophoet@localhost.localdomain>
2020-12-11 17:48:14 +0000

    (urls) - change the admin root url

--- comii/urls.py
+++ comii/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('da7c897187ad3abc987a/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 
     path('', include('core.urls')),
