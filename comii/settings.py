c75e823d15a4552ee7474f827a4c58a8def7549e
hophoet <hophoet@gmail.com>
2020-12-12 11:07:42 +0000

    (settings) - update project mode config with static production mode

--- comii/settings.py
+++ comii/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = False
 
 ALLOWED_HOSTS = ['hophoetcomii.herokuapp.com', '127.0.0.1']
 
